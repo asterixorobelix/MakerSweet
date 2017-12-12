68ed1baa9e8749395980e77c13959a725dc78420
nathan <nathanstasin@gmail.com>
2017-12-12 21:18:57 +0200

    added gcode file

---
+++ MakerSweet.Services/Models/GcodeFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+ * http://www.warrensbrain.com/gcode-to-english-translator.html
+ * https://machmotion.com/cnc-info/g-code.html
+ */
+
+namespace MakerSweet.Services.Models
+{
+    public class GcodeFile:File
+    {
+        public GcodeFile(string name) : base(name)
+        {
+            FileExtension = ".nc";
+        }
+    }
+}
