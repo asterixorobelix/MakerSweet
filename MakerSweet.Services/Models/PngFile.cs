39d1fac87a21095dccf90a6c12db944fcbc539ec
nathan <nathanstasin@gmail.com>
2017-12-13 21:30:39 +0200

    Added pngFile class

---
+++ MakerSweet.Services/Models/PngFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakerSweet.Services.Models
+{
+    public class PngFile:File
+    {
+        public PngFile(string name) : base(name)
+        {
+            FileExtension = ".png";
+            FullName = FileName + FileExtension;
+        }
+    }
+}
