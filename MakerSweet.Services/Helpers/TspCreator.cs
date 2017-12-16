c133e42234c8a74fffde60bc8966b000857508bd
nathan <nathanstasin@gmail.com>
2017-12-16 18:21:49 +0200

    added jpgFile class

--- MakerSweet.Services/Helpers/TspCreator.cs
+++ MakerSweet.Services/Helpers/TspCreator.cs
@@ -1,8 +1,6 @@
 ﻿using MakerSweet.Services.Models;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
 
 namespace MakerSweet.Services.Helpers
 {
---
+++ MakerSweet.Services/Models/JpgFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakerSweet.Services.Models
+{
+    public class JpgFile:File
+    {
+        public JpgFile(string name) : base(name)
+        {
+            FileExtension = ".jpg";
+            FullFileName = FileName + FileExtension;
+        }
+    }
+}
