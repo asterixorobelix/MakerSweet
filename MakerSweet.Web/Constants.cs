abc73a50af5c630215694c19136408a61913f2e1
nathan <nathanstasin@gmail.com>
2017-12-19 20:13:16 +0200

    Added jquery validation to _layout. Added onGet and OnPost methods. Added textboxes for model

---
+++ MakerSweet.Web/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MakerSweet.Web
+{
+    public static class Constants
+    {
+        public const string FILE_NAME_ERROR = "Please provide a file name";
+        public const string FILE_NAME_PLACEHOLDER = "FileName";
+        public const int STIPPLE_DEFAULT = 1000;
+        public const double SIZING_FACTOR_DEFAULT = 0.8;
+    }
+}
--- MakerSweet.Web/Pages/Stippler.cshtml.cs
+++ MakerSweet.Web/Pages/Stippler.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,9 +10,35 @@ namespace MakerSweet.Web.Pages
 {
     public class StipplerModel : PageModel
     {
-        public void OnGet()
+        public StipplerModel()
         {
-
+            StippleNumber = Constants.STIPPLE_DEFAULT;
+            SizingFactor = Constants.SIZING_FACTOR_DEFAULT;
         }
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return Page();
+        }
+        [BindProperty]
+        [Required(ErrorMessage =Constants.FILE_NAME_ERROR)]
+        public string FileName { get; set; }
+        [BindProperty]
+        [Required]
+        [Range(100,100000)]
+        public int StippleNumber { get; set; }
+        [BindProperty]
+        [Required]
+        [Range(0.1,20)]
+        public double SizingFactor { get; set; }
+        public bool NoOverlap { get; set; }
     }
 }
