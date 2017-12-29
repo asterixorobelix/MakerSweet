e4be94c9195702ad38cc8753dfb778ac1cd949d3
nathan <nathanstasin@gmail.com>
2017-12-29 21:06:23 +0200

    started on linifier

--- MakerSweet.Services/Helpers/ImageColorConverter.cs
+++ MakerSweet.Services/Helpers/ImageColorConverter.cs
@@ -12,12 +12,13 @@ namespace MakerSweet.Services.Helpers
         {
             try
             {
+                var threshold = 0.4f;// lower value leads to better results
                 using (Image<Rgba32> image =  Image.Load<Rgba32>($"{Constants.INPUTOUTPUT_FOLDER_RELATIVE_PATH}{filename}.png"))
-                {
-                    image.Mutate(x=>x.BlackWhite());
-                    image.Save($"{Constants.INPUTOUTPUT_FOLDER_RELATIVE_PATH}{filename}blackWhite.png");
+                {                    
+                    image.Mutate(x=>x.BinaryThreshold(threshold));//formerly image.Mutate(x=>x.BlackWhite()); but this produced a greyscale image
+                    image.Save($"{Constants.INPUTOUTPUT_FOLDER_RELATIVE_PATH}{filename}blackWhiteBinaryThreshold{threshold}.png");
                 }
-                    return $"{filename}BlackWhite.png";               
+                    return $"{filename}blackWhiteBinaryThreshold{threshold}.png";               
             }
             catch (Exception e)
             {
--- MakerSweet.Services/Helpers/Linifier.cs
+++ MakerSweet.Services/Helpers/Linifier.cs
@@ -8,15 +8,42 @@ namespace MakerSweet.Services.Helpers
 {
     public class Linifier:ILinifier
     {
+        private static int imageHeight { get; set; }
+        private static int imageWidth { get; set; }
+        private static int numberofPins { get; set; }
+        private static List<int> startingCoords { get; set; }
+        private static List<int> endingCoords { get; set; }
+        private static int count { get; set; }
+        private static int deltaHeight { get; set; }
+        private static int deltaWidth { get; set; }
+
         public string Linfiy(string filename, int numberOfLines, int numberOfPins)
         {
+            numberofPins = numberOfPins;
             try
             {
-                    //using (FileStream output = new FileStream(path: $"{Constants.INPUTOUTPUT_FOLDER_RELATIVE_PATH}{filename}LineNo{numberOfLines}PinNo{numberOfPins}.png", mode: FileMode.Create))
-
                 using (Image<Rgba32> image = Image.Load($"{Constants.INPUTOUTPUT_FOLDER_RELATIVE_PATH}{filename}.png"))
                 {
-                    image[200,200]=Rgba32.White;
+                    count = 0;
+                    imageHeight = image.Height; //height of image in pixels
+                    imageWidth = image.Width;
+                    startingCoords = randomCoords();
+                    endingCoords = randomCoords();                    
+
+                    var xCoord = startingCoords[0];
+                    var yCoord = startingCoords[1];
+
+                    while (xCoord != endingCoords[0])
+                    {
+                        evaluateIfPixelBlack(image, xCoord, startingCoords[1]);
+                        xCoord++;
+                    }
+
+                    while (yCoord != endingCoords[1])
+                    {
+                        evaluateIfPixelBlack(image, startingCoords[0], yCoord);
+                        yCoord++;
+                    }
                 }
                 return $"{filename}LineNo{numberOfLines}PinNo{numberOfPins}.png";
             }
@@ -24,6 +51,32 @@ namespace MakerSweet.Services.Helpers
             {
                 return $"{Constants.FAILURE} - Unable to find file: {filename}. {e.Message}";
             }
+        }
+
+        //returns a list containing two integers, generated randomly between imageheight and imagewidth
+        private static List<int> randomCoords()
+        {            
+            var random = new Random();
+            var randomHeight = random.Next(0, imageHeight+1);//int month = rnd.Next(1, 13); // creates a number between 1 and 12
+            var randomWidth = random.Next(0, imageWidth + 1);
+            var coords = new List<int> {randomHeight,randomWidth };
+            return coords;
+        }
+
+        private static void evaluateIfPixelBlack(Image<Rgba32> image,int xcoord,int ycoord)
+        {
+            var pixel = image[xcoord, ycoord];//eg: <1  1  1  1> <r g b a>
+
+            if (pixel == Rgba32.Black)//ie black pixel
+            {
+                count++;
+            }
+        }
+
+        private static void getDeltaHeightWidth()
+        {
+            deltaHeight =Math.Abs(startingCoords[0] - endingCoords[0]);
+            deltaWidth =Math.Abs(startingCoords[1] - endingCoords[1]);
         }
     }
 }
